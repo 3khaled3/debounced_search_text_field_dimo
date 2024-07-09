99e75c3254e648b4f49cbe7d2c55d90114650cac
3khaled3 <ff.tark123@gmail.com>
2024-07-10 01:33:50 +0300

    done

---
+++ android/app/src/main/kotlin/com/example/debounced_search_text_field_dimo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.debounced_search_text_field_dimo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'debounced_search_text_field_widget.dart';
+
+Future<void> main() async {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("demo"),
+      ),
+      body: Center(
+        child: DebouncedSearchTextField(
+          onSearchChanged: (value) {
+            print("Debounced $value");
+          },
+        ),
+      ),
+    );
+  }
+}
